5ceedf64d9f64bf1c4f0ba0e92881acd84dccffd
andres.mera <feli041089@gmail.com>
2023-12-22 08:38:09 -0500

    dev-cust : Creacion del service de customer y definiendo de los metodos.

---
+++ app-customer/src/main/java/com/project/appcustomer/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.project.appcustomer.service;
+
+import com.project.appcustomer.entity.Customer;
+import com.project.appcustomer.entity.Region;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CustomerService {
+    List<Customer> findCustomerAll();
+
+    List<Customer> findCustomerByRegion(Region region);
+
+    Customer createCustomer(Customer customer);
+
+    Customer updateCustomer(Long id, Customer customer);
+
+    Customer deleteCustomer(Customer customer);
+
+    Customer getCustumer(Long id);
+}
---
+++ app-customer/src/main/java/com/project/appcustomer/service/implementation/CustomerServiceImpl.java
@@ -0,0 +1,74 @@
+package com.project.appcustomer.service.implementation;
+
+import com.project.appcustomer.entity.Customer;
+import com.project.appcustomer.entity.Region;
+import com.project.appcustomer.repository.CustomerRepository;
+import com.project.appcustomer.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> findCustomerAll() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public List<Customer> findCustomerByRegion(Region region) {
+        return customerRepository.findByRegion(region);
+    }
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+        Customer customerDb = customerRepository.findByNumberId(customer.getNumeroId());
+        if (Objects.nonNull(customerDb))
+            return customerDb;
+
+        customer.setState("CREATED");
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer customer) {
+        Customer customerDb = getCustumer(id);
+        if (Objects.isNull(customerDb))
+            return null;
+
+        customerDb.setFirstName(customer.getFirstName());
+        customerDb.setLastName(customer.getLastName());
+        customerDb.setEmail(customer.getEmail());
+        customerDb.setPhotoUrl(customer.getPhotoUrl());
+
+        return customerRepository.save(customerDb);
+    }
+
+    @Override
+    public Customer deleteCustomer(Customer customer) {
+        Customer customerDb = getCustumer(customer.getId());
+        if (Objects.isNull(customerDb))
+            return null;
+
+        customerDb.setState("DELETE");
+
+        return customerRepository.save(customerDb);
+    }
+
+    @Override
+    public Customer getCustumer(Long id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+}
