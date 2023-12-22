28fabcd57f19264ee34dd407caefaced10106e92
andres.mera <feli041089@gmail.com>
2023-12-22 07:58:06 -0500

    dev-cust : Creacion del micro servicio de customer.

---
+++ app-customer/src/test/java/com/project/appcustomer/AppCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.appcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
