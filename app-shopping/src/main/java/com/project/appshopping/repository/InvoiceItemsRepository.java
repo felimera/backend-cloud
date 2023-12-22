b82525416606e9bc9e56556303fb99557660d2e4
andres.mera <feli041089@gmail.com>
2023-12-22 09:55:49 -0500

    dev-shop :  Creacion y definicion de algunas consultas del repository.

---
+++ app-shopping/src/main/java/com/project/appshopping/repository/InvoiceItemsRepository.java
@@ -0,0 +1,9 @@
+package com.project.appshopping.repository;
+
+import com.project.appshopping.entity.InvoiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem, Long> {
+}
---
+++ app-shopping/src/main/java/com/project/appshopping/repository/InvoiceRepository.java
@@ -0,0 +1,14 @@
+package com.project.appshopping.repository;
+
+import com.project.appshopping.entity.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+    List<Invoice> findByCustomerId(Long customerId);
+
+    Invoice findByNumberinvoice(String numberInvoice);
+}
