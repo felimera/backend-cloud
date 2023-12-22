df5d0d36f86df8f2e02f261d0dd9af9f72e397a6
andres.mera <feli041089@gmail.com>
2023-12-22 08:48:49 -0500

    dev-cust : Creacion del controller de Customer.java

---
+++ app-customer/src/main/java/com/project/appcustomer/controller/CustomerController.java
@@ -0,0 +1,109 @@
+package com.project.appcustomer.controller;
+
+import com.project.appcustomer.entity.Customer;
+import com.project.appcustomer.entity.Region;
+import com.project.appcustomer.exception.ResponseMessageException;
+import com.project.appcustomer.service.CustomerService;
+import com.project.appcustomer.utils.CadenaUtil;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+// -------------------Retrieve All Customers--------------------------------------------
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> listAllCustomers(@RequestParam(name = "regionId", required = false) Long regionId) {
+        List<Customer> customers;
+        if (null == regionId) {
+            customers = customerService.findCustomerAll();
+            if (customers.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+        } else {
+            Region region = new Region();
+            region.setId(regionId);
+            customers = customerService.findCustomerByRegion(region);
+            if (null == customers) {
+                log.error("Customers with Region id {} not found.", regionId);
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        return ResponseEntity.ok(customers);
+    }
+
+    // -------------------Retrieve Single Customer------------------------------------------
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id) {
+        log.info("Fetching Customer with id {}", id);
+        Customer customer = customerService.getCustomer(id);
+        if (null == customer) {
+            log.error("Customer with id {} not found.", id);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(customer);
+    }
+
+    // -------------------Create a Customer-------------------------------------------
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer customer, BindingResult result) {
+        log.info("Creating Customer : {}", customer);
+        if (result.hasErrors()) {
+            throw new ResponseMessageException("401-01", "Error in the attributes sent in the body of the post.", CadenaUtil.formatMessage(result), HttpStatus.BAD_REQUEST);
+        }
+
+        Customer customerDB = customerService.createCustomer(customer);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerDB);
+    }
+
+    // ------------------- Update a Customer ------------------------------------------------
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Object> updateCustomer(@PathVariable("id") long id, @RequestBody Customer customer) {
+        log.info("Updating Customer with id {}", id);
+
+        Customer currentCustomer = customerService.getCustomer(id);
+
+        if (null == currentCustomer) {
+            log.error("Unable to update. Customer with id {} not found.", id);
+            return ResponseEntity.notFound().build();
+        }
+        customer.setId(id);
+        currentCustomer = customerService.updateCustomer(id, customer);
+        return ResponseEntity.ok(currentCustomer);
+    }
+
+    // ------------------- Delete a Customer-----------------------------------------
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") long id) {
+        log.info("Fetching & Deleting Customer with id {}", id);
+
+        Customer customer = customerService.getCustomer(id);
+        if (null == customer) {
+            log.error("Unable to delete. Customer with id {} not found.", id);
+            return ResponseEntity.notFound().build();
+        }
+        customer = customerService.deleteCustomer(customer);
+        return ResponseEntity.ok(customer);
+    }
+}
