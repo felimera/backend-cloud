9f89e6ad43af455478ffc2bfd3d2e81701ea056e
andres.mera <feli041089@gmail.com>
2023-12-22 10:21:27 -0500

    dev-shop :  Creacion de la utilidad de cadenas.

---
+++ app-shopping/src/main/java/com/project/appshopping/utils/CadenaUtil.java
@@ -0,0 +1,27 @@
+package com.project.appshopping.utils;
+
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.project.appshopping.controller.ErrorMessage;
+import org.springframework.validation.BindingResult;
+
+import java.util.List;
+
+public class CadenaUtil {
+
+    private CadenaUtil() {
+        throw new IllegalStateException(CadenaUtil.class.toString());
+    }
+
+    public static String convertBodyToJson(String attributeName, Object body) {
+        JsonObject json = new JsonObject();
+        json.put(attributeName, body);
+        return json.toString();
+    }
+
+    public static List<ErrorMessage> formatMessage(BindingResult bindingResult) {
+        return bindingResult.getFieldErrors()
+                .stream()
+                .map(err -> ErrorMessage.builder().attributeName(err.getField()).attributeValue(err.getDefaultMessage()).build())
+                .toList();
+    }
+}
