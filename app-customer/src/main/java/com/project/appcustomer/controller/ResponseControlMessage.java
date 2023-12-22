5660686d884976ff0d17e4ae158a29f1cc4961de
andres.mera <feli041089@gmail.com>
2023-12-22 08:49:07 -0500

    dev-cust : Creacion del control de exceptiones.

---
+++ app-customer/src/main/java/com/project/appcustomer/controller/ResponseControlMessage.java
@@ -0,0 +1,14 @@
+package com.project.appcustomer.controller;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class ResponseControlMessage {
+    private String code;
+    private String message;
+    private List<ErrorMessage> errorMessages;
+}
---
+++ app-customer/src/main/java/com/project/appcustomer/exception/ResponseMessageException.java
@@ -0,0 +1,25 @@
+package com.project.appcustomer.exception;
+
+import com.project.appcustomer.controller.ErrorMessage;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ResponseMessageException extends RuntimeException {
+
+    private final String code;
+    private final String message;
+    private final List<ErrorMessage> errorMessages;
+    private final HttpStatus httpStatus;
+
+    public ResponseMessageException(String code, String message, List<ErrorMessage> errorMessages, HttpStatus httpStatus) {
+        this.code = code;
+        this.message = message;
+        this.errorMessages = errorMessages;
+        this.httpStatus = httpStatus;
+    }
+}
